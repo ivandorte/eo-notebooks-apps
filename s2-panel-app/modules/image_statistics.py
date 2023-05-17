1bae591061a6c6c2999042a17606df71d2959723
ivandorte <edgar_allan_poe88@hotmail.it>
2023-05-17 14:36:58 +0200

    Small fixes
    Signed-off-by: ivandorte <edgar_allan_poe88@hotmail.it>

--- s2-panel-app/modules/image_statistics.py
+++ s2-panel-app/modules/image_statistics.py
@@ -1,6 +1,8 @@
 import holoviews as hv
 import numpy as np
 import panel as pn
+from bokeh.models import WheelZoomTool
+from bokeh.models.formatters import NumeralTickFormatter
 
 
 def plot_s2_spindex_hist(event):
@@ -9,6 +11,17 @@ def plot_s2_spindex_hist(event):
     in a FloatPanel on button click.
     Solution by @Hoxbro: https://discourse.holoviz.org/t/how-to-display-a-floatpanel-on-button-click/5346/3
     """
+
+    def hook(plot, element):
+        """
+        Custom hook for disabling zoom on axis
+        """
+
+        # Disable zoom on axis
+        for tool in plot.state.toolbar.tools:
+            if isinstance(tool, WheelZoomTool):
+                tool.zoom_on_axis = False
+                break
 
     # Get spectral index data from the cache
     spindex_cache = pn.state.cache["spindex"]
@@ -24,15 +37,24 @@ def plot_s2_spindex_hist(event):
 
     # Plot the histogram
     spindex_hist = hv.Histogram((edges, frequencies)).opts(
-        xlabel=spindex_name, title=f"Histogram of {spindex_name} values"
+        xlabel=spindex_name,
+        yformatter=NumeralTickFormatter(format="0a"),
+        title=f"Histogram of {spindex_name} values",
+        hooks=[hook],
+        width=400,
+        height=400,
     )
+
+    # https://jspanel.de/#options
+    config = {
+        "resizeit": {"disable": "true"},
+        "headerControls": "closeonly",
+        "closeOnEscape": "true",
+    }
 
     # Embed the histogram in a FloatPanel
     float_hist = pn.layout.FloatPanel(
-        spindex_hist,
-        contained=False,
-        position="center",
-        margin=20,
+        spindex_hist, contained=False, position="center", margin=20, config=config
     )
 
     # Show the dialog
